b7246503b746ae48f102c8fda9b79513ddaa30e9
ilias <ilibtwee@gmail.com>
2021-12-27 13:22:48 +0100

    utility: value pieces added

--- project/options/UtilityOptions.py
+++ project/options/UtilityOptions.py
@@ -1,15 +1,12 @@
 from project.options.options import Options
 
 
-class ChessPiecesOptions(Options):
+class UtilityOptions(Options):
     def __init__(self):
         super().__init__()
-
-        # numerical value of each chess piece
         self.pawn_value = 10
         self.knight_value = 30
         self.bishop_value = 30
         self.rook_value = 50
         self.queen_value = 90
         self.king_value = 900
-
