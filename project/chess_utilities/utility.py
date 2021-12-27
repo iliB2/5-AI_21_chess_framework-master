08bc766c8c36469bc5b0b492840978426cfc8b8d
ilias <ilibtwee@gmail.com>
2021-12-27 14:24:21 +0100

    utility: value pieces added

--- project/chess_utilities/utility.py
+++ project/chess_utilities/utility.py
@@ -1,11 +1,13 @@
 from abc import ABC
 import chess
 
+from project.options.UtilityOptions import UtilityOptions
+
 """A generic utility class"""
 
 
 class Utility(ABC):
 
     # Determine the value of the current board position (high is good for white, low is good for black, 0 is neutral)
-    def board_value(self, board: chess.Board):
+    def board_value(self, board: chess.Board, options: UtilityOptions):
         pass
