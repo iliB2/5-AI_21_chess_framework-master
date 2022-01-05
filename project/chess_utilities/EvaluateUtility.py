6c89fe3c27b2bd81fe9289418b24adf549b2d2f9
mcornilly <79586304+mcornilly@users.noreply.github.com>
2022-01-05 17:07:05 +0100

    b

--- project/chess_utilities/EvaluateUtility.py
+++ project/chess_utilities/EvaluateUtility.py
@@ -2,56 +2,56 @@ import chess
 from project.chess_utilities.utility import Utility
 from project.options.EvaluateOptions import EvaluateOptions
 
+
 class EvaluateUtility(Utility):
     def __init__(self, options: EvaluateOptions) -> None:
         self.options = options
 
-
-    def getAbsoluteValue(self, board: chess.Board, x, y):
+    def getAbsoluteValue(self, board: chess.Board, x):
         if board.pieces(piece_type=chess.PAWN, color=chess.WHITE):
-            return 10 + (self.options.pawnEvalWhite[y][x])
+            return 10 + (self.options.pawnEvalWhite[x])
         elif board.pieces(piece_type=chess.PAWN, color=chess.BLACK):
-            return 10 + (self.options.pawnEvalBlack[y][x])
+            return 10 + (self.options.pawnEvalBlack[x])
         elif board.pieces(piece_type=chess.ROOK, color=chess.WHITE):
-            return 50 + (self.options.rookEvalWhite[y][x])
+            return 50 + (self.options.rookEvalWhite[x])
         elif board.pieces(piece_type=chess.ROOK, color=chess.BLACK):
-            return 50 + (self.options.rookEvalBlack[y][x])
+            return 50 + (self.options.rookEvalBlack[x])
         elif board.pieces(piece_type=chess.BISHOP, color=chess.WHITE):
-            return 30 + (self.options.BishopEvalWhite[y][x])
+            return 30 + (self.options.BishopEvalWhite[x])
         elif board.pieces(piece_type=chess.BISHOP, color=chess.BLACK):
-            return 30 + (self.options.BishopEvalBlack[y][x])
+            return 30 + (self.options.BishopEvalBlack[x])
         elif board.pieces(piece_type=chess.QUEEN, color=chess.WHITE):
-            return 90 + (self.options.evalQueen[y][x])
+            return 90 + (self.options.evalQueen[x])
         elif board.pieces(piece_type=chess.QUEEN, color=chess.BLACK):
-            return 90 + (self.options.evalQueen[y][x])
+            return 90 + (self.options.evalQueen[x])
         elif board.pieces(piece_type=chess.KING, color=chess.WHITE):
-            return 900 + (self.options.kingEvalWhite[y][x])
+            return 900 + (self.options.kingEvalWhite[x])
         elif board.pieces(piece_type=chess.KING, color=chess.BLACK):
-            return 900 + (self.options.kingEvalBlack[y][x])
+            return 900 + (self.options.kingEvalBlack[x])
 
-
-
-    def getPieceValue(self, board: chess.Board, x, y):
-        if board.pieces(piece_type=chess.PAWN, color=chess.WHITE) | board.pieces(piece_type=chess.ROOK, color=chess.WHITE | board.pieces(piece_type=chess.BISHOP, color=chess.WHITE) | board.pieces(piece_type=chess.QUEEN, color=chess.WHITE) | board.pieces(piece_type=chess.KING, color=chess.WHITE)):
-            absolutevalue = self.getAbsoluteValue(board, x, y)
-        elif board.pieces(piece_type=chess.PAWN, color=chess.BLACK) | board.pieces(piece_type=chess.ROOK, color=chess.BLACK | board.pieces(piece_type=chess.BISHOP, color=chess.BLACK) | board.pieces(piece_type=chess.QUEEN, color=chess.BLACK) | board.pieces(piece_type=chess.KING, color=chess.BLACK)):
-            absolutevalue = self.getAbsoluteValue(board, x, y)
+    def getPieceValue(self, board: chess.Board, x):
+        if board.pieces(piece_type=chess.PAWN, color=chess.WHITE) | board.pieces(piece_type=chess.ROOK,
+                                                                                 color=chess.WHITE | board.pieces(
+                                                                                         piece_type=chess.BISHOP,
+                                                                                         color=chess.WHITE) | board.pieces(
+                                                                                         piece_type=chess.QUEEN,
+                                                                                         color=chess.WHITE) | board.pieces(
+                                                                                         piece_type=chess.KING,
+                                                                                         color=chess.WHITE)):
+            absolutevalue = self.getAbsoluteValue(board, x)
+        elif board.pieces(piece_type=chess.PAWN, color=chess.BLACK) | board.pieces(piece_type=chess.ROOK,
+                                                                                   color=chess.BLACK | board.pieces(
+                                                                                           piece_type=chess.BISHOP,
+                                                                                           color=chess.BLACK) | board.pieces(
+                                                                                           piece_type=chess.QUEEN,
+                                                                                           color=chess.BLACK) | board.pieces(
+                                                                                           piece_type=chess.KING,
+                                                                                           color=chess.BLACK)):
+            absolutevalue = self.getAbsoluteValue(board, x)
         return absolutevalue
 
-
-
-
     def evaluateBoard(self, board: chess.Board):
-        totalEvaluation = 0
-        for i in range(8):
-            for k in range(8):
-                totalEvaluation = totalEvaluation + self.getPieceValue(board[i][k], i, k)
-        return totalEvaluation
-
-
-
-
-
-
-
-
+        totalevaluation = 0
+        for i in range(63):
+            totalevaluation = totalevaluation + self.getPieceValue(self, board.piece_at(i), i)
+        return totalevaluation
