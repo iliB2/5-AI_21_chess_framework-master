6b105da362e41625799c97aeab0d85cc599cfd57
mcornilly <79586304+mcornilly@users.noreply.github.com>
2022-01-09 18:38:07 +0100

    b

--- project/chess_utilities/EvaluateUtility.py
+++ project/chess_utilities/EvaluateUtility.py
@@ -11,27 +11,27 @@ class EvaluateUtility(Utility):
         if board.piece_at(x).piece_type == 1 & board.piece_at(x).color == chess.WHITE:
             return self.options.pawn_value + (self.options.pawnEvalWhite[x])
         elif board.piece_at(x).piece_type == 1 & board.piece_at(x).color == chess.BLACK:
-            return self.options.pawn_value + (self.options.pawnEvalBlack[x])
+            return -self.options.pawn_value + (self.options.pawnEvalBlack[x])
         elif board.piece_at(x).piece_type == 2 & board.piece_at(x).color == chess.WHITE:
             return self.options.knight_value + (self.options.KnightEval[x])
         elif board.piece_at(x).piece_type == 2 & board.piece_at(x).color == chess.BLACK:
-            return self.options.knight_value + (self.options.KnightEval[x])
+            return -self.options.knight_value + (self.options.KnightEval[x])
         elif board.piece_at(x).piece_type == 4 & board.piece_at(x).color == chess.WHITE:
             return self.options.rook_value + (self.options.rookEvalWhite[x])
         elif board.piece_at(x).piece_type == 4 & board.piece_at(x).color == chess.BLACK:
-            return self.options.rook_value + (self.options.rookEvalBlack[x])
+            return -self.options.rook_value + (self.options.rookEvalBlack[x])
         elif board.piece_at(x).piece_type == 3 & board.piece_at(x).color == chess.WHITE:
             return self.options.bishop_value + (self.options.BishopEvalWhite[x])
         elif board.piece_at(x).piece_type == 3 & board.piece_at(x).color == chess.BLACK:
-            return self.options.bishop_value + (self.options.BishopEvalBlack[x])
+            return -self.options.bishop_value + (self.options.BishopEvalBlack[x])
         elif board.piece_at(x).piece_type == 5 & board.piece_at(x).color == chess.WHITE:
             return self.options.queen_value + (self.options.evalQueen[x])
         elif board.piece_at(x).piece_type == 5 & board.piece_at(x).color == chess.BLACK:
-            return self.options.queen_value + (self.options.evalQueen[x])
+            return -self.options.queen_value + (self.options.evalQueen[x])
         elif board.piece_at(x).piece_type == 6 & board.piece_at(x).color == chess.WHITE:
             return self.options.king_value + (self.options.kingEvalWhite[x])
         elif board.piece_at(x).piece_type == 6 & board.piece_at(x).color == chess.BLACK:
-            return self.options.king_value + (self.options.kingEvalBlack[x])
+            return -self.options.king_value + (self.options.kingEvalBlack[x])
         else:
             return 0
 
